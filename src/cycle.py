d7d3991438ec51aa13eda087a4a31d646d1e7637
Richard Hull <rm_hull@yahoo.co.uk>
2013-03-02 22:58:58 +0000

    Don't hardcode the address

--- src/cycle.py
+++ src/cycle.py
@@ -5,9 +5,9 @@ import time
 
 bus = smbus.SMBus(1) # For revision 1 Raspberry Pi, change to bus = smbus.SMBus(1) for revision 2.
 
-address = 0x20 # I2C address of MCP23017
-bus.write_byte_data(0x20,0x00,0x00) # Set all of bank A to outputs
-bus.write_byte_data(0x20,0x01,0x00) # Set all of bank B to outputs
+address = 0x25 # I2C address of MCP23017
+bus.write_byte_data(address,0x00,0x00) # Set all of bank A to outputs
+bus.write_byte_data(address,0x01,0x00) # Set all of bank B to outputs
 register = 0x12 # = bank A, 0x13 = bank B
 
 # Handle the command line arguments
